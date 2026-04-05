fdfb79a8e11f6348dd567ef9173f9f8a169f4cc8
Nicki <rheedocs@outlook.com>
2026-04-05 16:24:06 +0200

    Projekt oprettet med Spring Boot, Thymeleaf, JDBC, MySQL og Spring Security

---
+++ src/main/java/dk/zealand/hw_racekattekluben/HwRacekatteklubenApplication.java
@@ -0,0 +1,13 @@
+package dk.zealand.hw_racekattekluben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwRacekatteklubenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwRacekatteklubenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/zealand/hw_racekattekluben/HwRacekatteklubenApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.zealand.hw_racekattekluben;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwRacekatteklubenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
