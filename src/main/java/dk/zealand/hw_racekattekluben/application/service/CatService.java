64ab2732fcaeb0541b8f8009e8e6755604095606
GoncaloLamy04 <god001@edu.zealand.dk>
2026-04-12 17:47:23 +0200

    Cat Service oprettet med metoderne

--- src/main/java/dk/zealand/hw_racekattekluben/application/service/CatService.java
+++ src/main/java/dk/zealand/hw_racekattekluben/application/service/CatService.java
@@ -2,6 +2,7 @@ package dk.zealand.hw_racekattekluben.application.service;
 
 import dk.zealand.hw_racekattekluben.application.interfaces.ICatRepository;
 import dk.zealand.hw_racekattekluben.domain.Cat;
+import dk.zealand.hw_racekattekluben.domain.exceptions.CatNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,24 +16,43 @@ public class CatService {
         this.catRepository = catRepository;
     }
 
-    public List<Cat> getAll() {
-        return null;
+    public List<Cat> getAll() { return catRepository.findAll();
     }
 
     public Cat getById(int id) {
-        return null;
+        if (id <= 0){
+            throw new IllegalArgumentException("Id skal være større end 0!");
+        }
+        Cat cat = catRepository.findById(id);
+        if (cat == null) throw new CatNotFoundException(id);
+        return cat;
     }
 
     public List<Cat> getByMemberId(int memberId) {
-        return null;
+        if (memberId <= 0){throw new IllegalArgumentException("Member id skal være større end 0");}
+        List<Cat> cats = catRepository.findByMemberId(memberId);
+        if (cats == null || cats.isEmpty()){
+            throw new IllegalArgumentException("Ingen katte blev fundet fo denne member id: " + memberId);
+        }
+        return cats;
+    }
+
+    public void catValidate(Cat cat){
+        if (cat.getName() == null || cat.getBreederName() == null) {throw new IllegalArgumentException("Navn og Breeder navn må ikke være tomme");}
+        if (cat.getDeathdate() == null || cat.getBirthdate() == null){throw new IllegalArgumentException("Death datoen og Birth datoen må ikke være tomme");}
     }
 
     public void create(Cat cat) {
+        catValidate(cat);
+        catRepository.save(cat);
     }
 
     public void update(Cat cat) {
+        catValidate(cat);
+        catRepository.update(cat);
     }
 
     public void delete(int id) {
+        catRepository.delete(id);
     }
 }
